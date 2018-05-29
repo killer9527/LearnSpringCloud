68dd973f87c81779e26bdd296ceffea4da7da9cd
wangfuwei <wangfuwei@gridsum.com>
2018-05-29 18:04:40 +0800

    添加Hystrix DashBoard

--- wfw-feign-consumer/src/main/java/com/wfw/feign/consumer/WfwFeignConsumerApplication.java
+++ wfw-feign-consumer/src/main/java/com/wfw/feign/consumer/WfwFeignConsumerApplication.java
@@ -1,16 +1,36 @@
 package com.wfw.feign.consumer;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
 public class WfwFeignConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WfwFeignConsumerApplication.class, args);
+	}
+
+	/**
+	 * 否则会出现“Unable to connect to Command Metric Stream”
+	 * @return
+	 */
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+
 	}
 }
--- wfw-ribbon-consumer/src/main/java/com/wfw/ribbon/consumer/WfwRibbonConsumerApplication.java
+++ wfw-ribbon-consumer/src/main/java/com/wfw/ribbon/consumer/WfwRibbonConsumerApplication.java
@@ -1,25 +1,44 @@
 package com.wfw.ribbon.consumer;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableHystrixDashboard
 public class WfwRibbonConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WfwRibbonConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WfwRibbonConsumerApplication.class, args);
+    }
 
-	@Bean
-	@LoadBalanced
-	RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    /**
+     * 否则会出现“Unable to connect to Command Metric Stream”
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
